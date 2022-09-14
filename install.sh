29b33a2657f33d22caf3f42a610c0ceaccbcf9a0
Federico De Giuli <fdegiuli@grailbio.com>
2022-09-14 16:43:36 -0700

    Update install script to install a bunch of homebrew stuff

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Running install script from $DIR"
 
-echo "export DOTFILES=$DIR" >> ~/.bash_profile
-echo "source $DIR/.bash_profile" >> ~/.bash_profile
+echo "export DOTFILES=$DIR" >> ~/.profile
+echo "source $DIR/.bash_profile" >> ~/.profile
+
+# install Homebrew and use it get a bunch of things
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle install --file=$DIR/brewfile
 
 # Download the git completion files
 curl -LJO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh 
 curl -LJO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
-brew install thefuck
-brew install bash-completion@2
 
 echo "Linking .inputrc"
 ln -s $DIR/.inputrc ~/.inputrc
